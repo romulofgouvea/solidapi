0beffc6d2628dfa36790a31605ed0ee78ab1292a
Vinicius <viniciustavarespimenta@gmail.com>
2021-05-25 15:56:08 -0300

    docs: add comments

--- src/useCases/CreateUser/index.ts
+++ src/useCases/CreateUser/index.ts
@@ -38,3 +38,14 @@ export { createUserUseCase, createUserController };
 //   [x] o ICreateUserRequestDTO tem a única responsabilidade de determinar como vai ser
 //   a transmissão dessa mensagem entre o controller e o useCase
 //   etc
+
+// Entities são as models, mas nem toda entiites são representações de tabelas no banco de dados.
+
+// Repositories são classes especificas para a gente fazer a comunicação entre as funcionalidades 
+// da aplicação com o banco de dados, com a estrutura que a gente vai armazenar esses dados.
+
+// Os useCases baseados até no diagrama de classe de uso que a gente aplica 
+// lá na Engenharia de Software são as ações que um determinado tipo de usuário 
+// pode realizar dentro da nossa aplicação. A gente pode pensar que cada caso de uso 
+// da nossa aplicação como uma funcionalidade que o usuário pode fazer aqui dentro. 
+// A única funcionalidade que a gente tem até agora na nossa API é a criação de um usuário.
